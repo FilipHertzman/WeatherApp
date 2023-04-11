436984ef0b00af9ac33c3e531df794808ac74b65
filiphertzman <filip.hertzman@gmail.com>
2023-04-11 15:15:50 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Filip Hertzman on 2023-04-11.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
